748b38701d80454ce4696af065cf68b1a0d7a2c7
teddylong <teddy_lee11@hotmail.com>
2014-06-25 09:40:53 +0800

    First

---
+++ CSharpAndJavaScript/Data.ashx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace CSharpAndJavaScript
+{
+    /// <summary>
+    /// Summary description for Data
+    /// </summary>
+    public class Data : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            
+            //string result = "[{"\""id"\"":"\""112824"\"",'firstname':'sdf','lastname':'sdf','phone':'fsdf','email':'sdf@ads.ds'}]";
+            StringBuilder jsonBuilder = new StringBuilder();
+            jsonBuilder.Append("[");
+            jsonBuilder.Append("{");
+            jsonBuilder.Append("\"");
+            jsonBuilder.Append("id");
+            jsonBuilder.Append("\":\"12345");
+            jsonBuilder.Append("\",");
+
+            jsonBuilder.Append("\"");
+            jsonBuilder.Append("firstname");
+            jsonBuilder.Append("\":\"sdf");
+            jsonBuilder.Append("\",");
+
+            jsonBuilder.Append("\"");
+            jsonBuilder.Append("lastname");
+            jsonBuilder.Append("\":\"sdf");
+            jsonBuilder.Append("\",");
+
+            jsonBuilder.Append("\"");
+            jsonBuilder.Append("phone");
+            jsonBuilder.Append("\":\"12345");
+            jsonBuilder.Append("\",");
+
+            jsonBuilder.Append("\"");
+            jsonBuilder.Append("email");
+            jsonBuilder.Append("\":\"sdf@ads.ds");
+            jsonBuilder.Append("\"");
+
+            jsonBuilder.Append("}]");
+
+            context.Response.Clear();
+            context.Response.Write(jsonBuilder.ToString());
+            context.Response.End();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CSharpAndJavaScript/Index.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="CSharpAndJavaScript.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>WhatEver</title>
+    <link rel="stylesheet" type="text/css" href="jQueryEasy/easyui.css" />
+    <link rel="stylesheet" type="text/css" href="jQueryEasy/icon.css" />
+    <link rel="stylesheet" type="text/css" href="jQueryEasy/demo.css" />
+    <script type="text/javascript" src="jQueryEasy/jquery.min.js"></script>
+    <script type="text/javascript" src="jQueryEasy/jquery.easyui.min.js"></script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Basic DataGrid</h2>
+            
+            <div style="margin: 20px 0;"></div>
+
+            <table class="easyui-datagrid"  title="Basic DataGrid" style="width: 700px; height: 250px"
+                data-options="singleSelect:true,collapsible:true,url:'Data.ashx'" >
+                <thead>          
+                     <tr>
+                        <th data-options="field:'id',width:120">ID</th>
+                        <th data-options="field:'firstname',width:120">First Name</th>
+                        <th data-options="field:'lastname',width:120,align:'right'">Last Name</th>
+                        <th data-options="field:'phone',width:140,align:'right'">Phone</th>
+                        <th data-options="field:'email',width:193">Email</th>        
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
