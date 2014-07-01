6dedff2831fc5c068afb575e156637bb5d473e71
teddylong <teddy_lee11@hotmail.com>
2014-07-01 18:03:25 +0800

    asdd

---
+++ CSharpAndJavaScript/jQueryEasy/Mobile.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+
+
+    
+    //$('#TaskPanel').hide();
+    $('#addBtn').bind('click', function () {
+        $('#AddPanel').hide();
+        $('#TaskPanel').show();
+    });
+
+    $('#CancelBtn').bind('click', function () {
+        $('#AddPanel').show();
+        $('#TaskPanel').hide();
+    });
+});
